61de66637eb51e33d1c8da47eff9bbd97f162f08
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-07-27 11:25:11 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		double sum = 0;
+		double max = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i=0;i<N;i++) {
+			sum += (int)arr[i];
+			if(max<arr[i]) {
+				max = (int)arr[i];
+				
+			}
+		}
+		System.out.println(sum/max*100/N);
+	}
+}
