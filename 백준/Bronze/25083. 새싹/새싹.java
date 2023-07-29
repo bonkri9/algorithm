af12b7ff42cd4b7e5852614e79c719d5681b7646
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-07-29 22:43:52 +0900

    [Bronze V] Title: 새싹, Time: 200 ms, Memory: 17404 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+		
+		
+	}
+
+}
