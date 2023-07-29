ac18126a77b2270b427354526107954fbb2addfd
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-07-29 13:14:44 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 396 ms, Memory: 18680 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i ; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < 2 * N - (2 * (i + 1) - 1); k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
