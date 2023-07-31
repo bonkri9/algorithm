0c8744db810d490cf855d35bb0ddcc1de844412a
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-07-31 22:23:25 +0900

    [Bronze II] Title: 피보나치 수, Time: 216 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] arr = new int[46];
+		
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		for(int i=2;i<=N;i++) {
+			arr[i] = arr[i-1]+arr[i-2];
+		}
+		System.out.println(arr[N]);
+		
+	}
+	
+}
