9fe6a93e472b9dea7f0f6ba2bf23eb265c884795
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-01 13:49:15 +0900

    [Bronze V] Title: A+B - 9, Time: 376 ms, Memory: 21068 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger A = new BigInteger(sc.next());
+		BigInteger B = new BigInteger(sc.next());
+		
+		
+		System.out.println(A.add(B));
+		
+		
+	}
+
+}
