48d15c7fe97fb66c428aed3a8360ae6dadc6102b
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-02 12:01:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 116 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
+
 		int year = sc.nextInt();
-		
-		System.out.println(year -543);
+
+		System.out.println(year - 543);
+
 	}
 
 }
