998bf52eac9695b6ab215ec31648a7a60dea58ae
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-02 11:48:09 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 12968 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		double a = sc.nextInt();
-		double b = sc.nextInt();
-		System.out.println(a/b);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		System.out.println((double)A / B);
+
 	}
+
 }
