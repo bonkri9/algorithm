26ac2fedde4a1bc65445f9fee241c4f6abbc602b
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-02 13:35:30 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11284 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+
 		System.out.println("\\    /\\");
 		System.out.println(" )  ( ')");
 		System.out.println("(  /  )");
 		System.out.println(" \\(__)|");
+
 	}
+
 }
