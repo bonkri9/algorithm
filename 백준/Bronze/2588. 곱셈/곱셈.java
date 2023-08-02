1823a6d69a9f66f27ba6cc5b9081b982ba84ec44
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-02 13:10:41 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		
-		int one = B-(B/10)*10;
-		int ten = (B/10)-(B/100)*10;
-		int hund = B/100;
-		
-		System.out.println(A*one);
-		System.out.println(A*ten);
-		System.out.println(A*hund);
-		System.out.println(A*B);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a * (b % 10));
+		System.out.println(a * ((b / 10) % 10));
+		System.out.println(a * (b / 100));
+		System.out.println(a * b);
+
 	}
+
 }
