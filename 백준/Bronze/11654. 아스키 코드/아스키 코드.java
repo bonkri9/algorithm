9a12761deb2cceefbb8183b90079df06d7dc91c2
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-07 14:51:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char A = sc.nextLine().charAt(0);
+		
+		int N = (int) A;
+		
+		System.out.println(N);
+		
+		
+	}
+
+}
