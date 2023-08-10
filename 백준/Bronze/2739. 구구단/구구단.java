a89692421a34c5959c5a6e80800ce42b186ce1eb
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-10 10:02:43 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int i;
-		for(i=1; i<=9; i++) {
-			System.out.print(a);
-			System.out.print(" * ");
-			System.out.print(i);
-			System.out.print(" = ");
-			System.out.println(a*i);
-			
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(N+" * "+i+" = " + N * i);
 		}
 	}
 }
