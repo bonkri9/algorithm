1eb4aae932e25c9d9de30d2d1e0dde8640af1b3f
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-10 13:10:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 13908 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int i;
-		for(i=1;i<=a;i++) {
-			int j;
-		
-			for(j=0;j<i;j++) {
+
+		int N = sc.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
-			System.out.println("");
+			System.out.println();
 		}
-		
-
 	}
-
 }
