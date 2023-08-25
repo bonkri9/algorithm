74d1bedd76207d5633d34398310ccfdf709064f3
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-08-25 15:57:24 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 208 ms, Memory: 17624 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] mushroom = new int[10];
-		for (int i = 0; i < 10; i++) {
-			mushroom[i] = sc.nextInt();
-		}
-
+		
+		int tmp = 0;
 		int sum = 0;
-		for (int i = 0; i < 10; i++) {
-			if (Math.abs(100 - sum) >= Math.abs(100 - sum - mushroom[i])) {
-				sum += mushroom[i];
-			} else
+		
+		for(int i=0; i<10; i++) {
+			tmp = sc.nextInt();
+			sum += tmp;
+			
+			if (sum >= 100) {
 				break;
+			}
+		}
+		
+		int bef = sum - tmp;
+		
+		if ( 100-bef < sum-100 ) {
+			sum = bef;
 		}
 		System.out.println(sum);
-
-	}// main
+	}
 }
