961635c1cc9e954c80220820925212348f99e291
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2023-10-19 13:53:12 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 136 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -8,50 +8,45 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+//문자열 1316 그룹 단어 체커
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			String[] arrStr = str.split("");
-			boolean[] alphabet = new boolean[26];
-			alphabet[str.charAt(0) - 'a'] = true;
+		int N = Integer.parseInt(br.readLine()); //총 단어의 갯수
+		int cnt = 0; //그룹 단어의 갯수
+		
+		for (int i = 0; i < N; i++) { //단어 갯수만큼 반복문 실행
+			String str = br.readLine(); //단어 입력
+			String[] arrStr = str.split(""); //단어를 낱말마다 쪼개서 배열에 저장
+			boolean[] alphabet = new boolean[26]; //알파벳 갯수만큼의 boolean 배열
+			alphabet[str.charAt(0) - 'a'] = true; //가장 첫 낱말은 true로 표시
 
-//			System.out.println(Arrays.toString(alphabet));
-//			System.out.println(Arrays.toString(arrStr));
+			if (str.length() == 1) //만약 단어의 낱말 갯수가 1개라면 아래의 반복문 돌지 않게 되지만
+				cnt++;             //그룹단어 조건에 만족하므로 단어갯수 +1
 
-			if (str.length() == 1)
-				cnt++;
-
-			for (int j = 1; j < str.length(); j++) {
-				int idx = str.charAt(j) - 'a';
-				if (!alphabet[idx]) {
-					alphabet[idx] = true;
-				} else if (arrStr[j].equals(arrStr[j - 1])) {
-					if (j == str.length() - 1)
-						cnt++;
-					continue;
-				} else if (!(arrStr[j].equals(arrStr[j - 1]))) {
-					break;
+			for (int j = 1; j < str.length(); j++) { //단어의 낱말 갯수가 2 이상인 것만 반복문 실행
+				int idx = str.charAt(j) - 'a'; //낱말에 해당하는 알파벳 자리를 idx에 저장
+				if (!alphabet[idx]) { //만약 해당 알파벳이 아직 방문하지 않아 false라면
+					alphabet[idx] = true; //true로 방문 처리
+				} else if (arrStr[j].equals(arrStr[j - 1])) { //방문한 것중 직전 낱말과 같은 낱말이라면
+					if (j == str.length() - 1) //게다가 그 낱말이 마지막 낱말이라면, 다음 반복문은 돌지 않는다
+						cnt++; //갯수 추가
+					continue; //직전 낱말과 같은 낱말이면 패스
+				} else if (!(arrStr[j].equals(arrStr[j - 1]))) { //방문한 낱말인데, 직전 낱말과 다른 낱말이면
+					break; //그룹단어 조건에 만족하지 않으므로 반복문 그만 돌기
 				}
-				if (j == str.length() - 1) {
-					cnt++;
+				if (j == str.length() - 1) { //마지막 낱말이라면
+					cnt++; //반복문 안전하게 모두 돈 것이므로 단어 갯수 추가
 				}
-
-//				System.out.println(idx);
-//				System.out.println(Arrays.toString(alphabet));
 
 			}
 
 		}
-//		System.out.println(cnt);
+
 		br.close();
-		bw.append(String.valueOf(cnt));
+		bw.append(String.valueOf(cnt)); //그룹 단어 조건에 만족하는 단어 갯수 출력
 		bw.flush();
 		bw.close();
 
