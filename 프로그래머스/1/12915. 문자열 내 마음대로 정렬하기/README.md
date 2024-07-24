36625ffa80bec738b430f4f8933f0310bfd111d1
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-24 11:41:03 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.46 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 마음대로 정렬하기 - 12915 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12915) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.57 ms
+메모리: 73.5 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 24일 11:31:49
+2024년 07월 24일 11:41:02
 
 ### 문제 설명
 
--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -4,28 +4,34 @@ class Solution {
     public String[] solution(String[] strings, int n) {
         int stringsArrLen = strings.length;
         
+        /* 
+        마지막에 인덱스 n의 문자가 같은 문자열이 여럿 일 경우
+        문자열이 사전순으로 정렬되어야 하므로
+        미리 전체를 사전순으로 정렬
+        */
         Arrays.sort(strings);
         
+        // 알파벳 갯수는 26개 , ArrayList 생성 및 초기화
         ArrayList<String>[] arr = new ArrayList[26];
-        
-        for(int i=0;i<26;i++){
+        for(int i = 0; i < 26; i++){
             arr[i] = new ArrayList<String>();
         }
         
-        for(int i=0;i<stringsArrLen;i++){
+        // 기준이 되는 인덱스 n의 문자를 삽입
+        for(int i = 0; i < stringsArrLen; i++){
             int stdNum = strings[i].charAt(n) - 'a';
             arr[stdNum].add(strings[i]);
         }
         
-        
         String[] answer = new String[stringsArrLen];
-        int idx =0;
+        int idx = 0;
         
-        for(int i=0;i<26;i++){
-            for(int j=0;j<arr[i].size();j++){
+        for(int i = 0; i < 26; i++){
+            for(int j = 0; j < arr[i].size(); j++){
                 answer[idx++] = arr[i].get(j);
             }
         }
+        
         return answer;
     }
 }
