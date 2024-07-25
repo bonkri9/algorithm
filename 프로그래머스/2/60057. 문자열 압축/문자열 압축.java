411bdba8ba518c857dc51343375a48e68953c70b
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-26 01:05:04 +0900

    [level 2] Title: 문자열 압축, Time: 12.41 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -4,10 +4,14 @@ import java.io.*;
 class Solution {
     public static String compressor(String str, int unit){
         StringBuilder sb = new StringBuilder();
+        
+        // 잘려진 문자가 먼저 나가는게 FIFO 인 Queue가 적합하다고 판단 
         Queue<String> q = new LinkedList<>();
         
-        for(int i=0;i<str.length();i+=unit){
-            int endIdx = Math.min(i+unit, str.length());
+        // 자르는 단위를 토대로 substring의 끝 인덱스를 지정
+        // 자른 뒤 Queue에 넣기 
+        for(int i = 0; i < str.length(); i += unit){
+            int endIdx = Math.min(i + unit, str.length());
             q.offer(str.substring(i, endIdx));
         }
         
@@ -15,11 +19,13 @@ class Solution {
             int cnt = 1;
             String curr = q.poll();
             
+            // 잘려진 현재 문자열과 같다면 cnt 1 증가 및 queue에서 버리기 
             while(!q.isEmpty() && q.peek().equals(curr)){
                 q.poll();
                 cnt++;
             }
             
+            // 한번만 나타난 경우는 1을 생략하고 문자열 붙이기 
             if(cnt > 1){
                 sb.append(cnt).append(curr);
             }else{
@@ -32,10 +38,12 @@ class Solution {
     }
     
     public int solution(String s) {
+        // 주어진 문자열의 길이 저장 및 최소 길이 초기화
         int sLen = s.length();
         int minLen = sLen;
         
-        for(int i = 1; i <= sLen/2; i++){
+        // 자르는 단위 i는 1글자부터 최대 (문자열 길이) / 2 이어야 유의미
+        for(int i = 1; i <= sLen / 2; i++){
             String compStr = compressor(s, i);
             minLen = Math.min(minLen, compStr.length());
         }
