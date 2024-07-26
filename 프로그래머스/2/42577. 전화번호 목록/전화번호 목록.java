ea76276bef6d48e7fa982db6defbe883d888b378
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-26 15:22:10 +0900

    [level 2] Title: 전화번호 목록, Time: 320.30 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        
+        for(int i=0;i<phone_book.length-1;i++){
+            String curr = phone_book[i];
+            String next = phone_book[i+1];
+            if(next.startsWith(curr)){
+                answer = false;
+            }            
+        }
+        
+        
+        return answer;
+    }
+}
