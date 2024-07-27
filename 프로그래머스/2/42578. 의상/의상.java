6079017e16f7bafca3ec2503071e4faae9fa7cf6
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-27 22:46:51 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> mapCategory = new HashMap<>();
+        
+        // 의상 종류에 따른 갯수 세기
+        for(int i = 0; i < clothes.length; i++){
+            mapCategory.put(clothes[i][1], mapCategory.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        // key 데이터를 순회하면서 각 의상 종류의 갯수에 따른 계산
+        for(String key : mapCategory.keySet()){
+            answer *= mapCategory.get(key) + 1;
+        }
+        
+        // 의상을 모두 안 입는 조건인 1 빼기
+        return answer - 1;
+    }
+}
