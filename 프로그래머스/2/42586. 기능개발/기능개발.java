549d172d7ae0990abe46d0d2b23b857feb5a9a82
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-29 13:05:54 +0900

    [level 2] Title: 기능개발, Time: 0.26 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,20 +4,24 @@ import java.io.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
+        // 최종 답안을 잠시 넣을 tmp 
         ArrayList<Integer> tmp = new ArrayList<>();
         
+        // 기능마다의 작업 수행 기간을 담을 queue
         Queue<Integer> qDuration = new LinkedList<>();
         for(int i=0;i<progresses.length;i++){
             int duration = (100 - progresses[i]) / speeds[i];
             int rest = (100 - progresses[i]) % speeds[i];
-
+            
+            // 나누어떨어지지 않으면 1일 추가 
             if(rest != 0){
                 qDuration.add(duration + 1);
             }else{
                 qDuration.add(duration);
             }
         }
-
+        
+        // 배포마다의 기능 갯수
         int serviceCnt = 1;
         int curr = qDuration.poll();
 
@@ -30,11 +34,8 @@ class Solution {
                 serviceCnt = 1;
                 curr = qDuration.poll();
             }
-            
-            
-            
         }
-        tmp.add(serviceCnt);
+        tmp.add(serviceCnt); // 마지막 서비스 갯수 추가 
 
         int[] answer = new int[tmp.size()];
         for(int i=0;i<answer.length;i++){
