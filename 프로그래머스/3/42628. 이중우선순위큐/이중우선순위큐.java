41dcf6b414433e0c433824c44760dc9d4f3bf5f6
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-07-31 11:52:00 +0900

    [level 3] Title: 이중우선순위큐, Time: 80.38 ms, Memory: 134 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,18 +3,25 @@ import java.io.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-
+        
+        // 최솟값 우선순위 minPq , 최댓값 우선순위 maxPq
         PriorityQueue<Integer> minPq = new PriorityQueue<>();
         PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
 
-        int commandCnt = operations.length;
-        String[] command = new String[2];
-        for(int i=0;i<commandCnt;i++){
+        int commandCnt = operations.length; // 명령 횟수
+        String[] command = new String[2]; // 명령이 띄어쓰기로 담길 배열
+        
+        for(int i = 0; i < commandCnt; i++){
+            
             command = operations[i].split(" ");
-            if(command[0].equals("I")){
+            
+            if(command[0].equals("I")){ // 삽입 명령
+                
                 minPq.offer(Integer.parseInt(command[1]));
                 maxPq.offer(Integer.parseInt(command[1]));
-            }else{
+                
+            }else{ // 제거 명령
+                
                 if(command[1].equals("1") && !maxPq.isEmpty()){
                     int max = maxPq.poll();
                     minPq.remove(max);
@@ -22,14 +29,15 @@ class Solution {
                     int min = minPq.poll();
                     maxPq.remove(min);
                 }
+                
             }
 
         }
-
+        
+        // 최댓값, 최솟값 순으로 담길 answer 배열
         int[] answer = new int[2];
         answer[0] = !maxPq.isEmpty() ? maxPq.poll() : 0;
         answer[1] = !minPq.isEmpty() ? minPq.poll() : 0;
-
 
         return answer;
     }
