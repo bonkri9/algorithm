4b4bf36e82be9c46e5f3b8e0d22e944e6127c31f
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-08-05 09:56:01 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1024 ms, Memory: 172792 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -12,16 +12,20 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
             int key = Integer.parseInt(st.nextToken());
-            hashMap.put(key, hashMap.getOrDefault(key, 0)+1);
+            
+            // hashMap의 key에 해당하는 value를 다음 값으로 넣어라
+            // key와 맵핑된 value 값을 반환하고 없으면 기본값 0을 반환
+            hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
         }
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i=0;i<m;i++){
+        for(int i = 0; i < m; i++){
             int target = Integer.parseInt(st.nextToken());
-
+            
+            // target에 맵핑된 value 값을 반환하고, 없으면 0 반환 
             sb.append(hashMap.getOrDefault(target, 0)).append(" ");
         }
 
