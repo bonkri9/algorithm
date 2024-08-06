1a6110140bbe815f1cf60074c8218d7dedbe90d6
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-08-06 14:10:47 +0900

    [level 2] Title: 모음 사전, Time: 14.71 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,30 @@
 import java.util.*;
 
 class Solution {
-    public static String[] vowels = {"A", "E","I", "O", "U"};
+    public static String[] vowels = {"A", "E", "I", "O", "U"};
     public static final int MAX_LENGTH = 5;
     public static List<String> dict;
     
+    // 단어 삽입 dfs 
     public static void wordInsert(String str, int depth){
         dict.add(str);
         
+        // 최대 길이에 도달하면 return 
         if(depth == MAX_LENGTH) return;
         
-        for(int i=0;i<MAX_LENGTH;i++){
+        // 모음 하나씩 붙여가며 단어 길이 1 증가 
+        for(int i = 0; i < MAX_LENGTH; i++){
             wordInsert(str + vowels[i], depth + 1);
         }
         
     }
     
     public int solution(String word) {
+        // 사전 생성
         dict = new ArrayList<>();
         
+        // 0 번째 단어가 "", 1 번째 단어가 A
+        // 첫 시작 단어 길이는 0 
         wordInsert("", 0);
         
         
@@ -29,6 +35,7 @@ class Solution {
                 break;
             }
         }
+        
         return answer;
     }
 }
