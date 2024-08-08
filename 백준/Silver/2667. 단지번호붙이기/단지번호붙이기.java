86fe4472ed9fcdd5d49bfdf2f4be62bfd2b89651
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-08-08 11:19:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 148 ms, Memory: 16068 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	static int N, r, c, cnt;
