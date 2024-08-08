528aa49ca2d007c06b794a7a87d19905bac6ec7e
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-08-08 13:23:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,19 +2,17 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, r, c, cnt;
-	static String[][] map;
-	static boolean[][] visit;
-	static int[] dr = { 0, 0, -1, 1 };
-	static int[] dc = { -1, 1, 0, 0 };
-	static int[] group;
+	public static int N, cnt;
+	public static String[][] map;
+	public static boolean[][] visit;
+	public static int[] dr = { 0, 0, -1, 1 };
+	public static int[] dc = { -1, 1, 0, 0 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
+		
 		N = Integer.parseInt(br.readLine()); // 지도의 한 변
 
 		map = new String[N][N]; // 지도 생성
@@ -30,7 +28,9 @@ public class Main {
 		for (int i = 0; i < N; i++) { // 행
 			for (int j = 0; j < N; j++) { // 열
 				cnt = 0; // 한 단지 내 가구 수
-				if (!visit[i][j] && map[i][j].equals("1")) { // 방문하지 않았고, 아파트가 있다면
+                
+                // 방문하지 않았고, 아파트가 있다면
+				if (!visit[i][j] && map[i][j].equals("1")) { 
 					DFS(i, j); // 깊이 우선 탐색(행,열)
 					list.add(cnt); // 단지 내 가구 수를 리스트에 입력
 				}
@@ -39,9 +39,9 @@ public class Main {
 
 		Collections.sort(list); // 리스트 오름차순 정렬
 
-		sb.append(list.size() + "\n"); // 리스트 사이즈가 곧 총 단지 수
-		for (int i = 0; i < list.size(); i++) { // 한 단지 내 가구 수 출력
-			sb.append(list.get(i) + "\n");
+		sb.append(list.size()).append("\n"); // 리스트 사이즈가 곧 총 단지 수
+		for (int cnt : list) { // 한 단지 내 가구 수 출력
+			sb.append(cnt).append("\n");
 		}
 
 		br.close();
@@ -57,9 +57,13 @@ public class Main {
 		for (int i = 0; i < 4; i++) {
 			int nr = r + dr[i]; // 델타로 위치 갱신 , 행
 			int nc = c + dc[i]; // 델타로 위치 갱신 , 열
-			if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visit[nr][nc] && map[nr][nc].equals("1")) {
-				DFS(nr, nc); // 경계 안에 있고, 방문하지 않았고, 아파트가 있다면
-				// 다시 DFS 재귀
+            
+            // 경계 안에 있고, 방문하지 않았고, 아파트가 있다면
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N 
+            		&& !visit[nr][nc] && map[nr][nc].equals("1")) {
+                
+                // 다시 DFS 재귀
+				DFS(nr, nc); 
 			}
 		}
 	}
