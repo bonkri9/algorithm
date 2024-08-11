7eab5e987616e614ea71a62a7558020daebd96aa
Bonmin Ku <139411343+bonkri9@users.noreply.github.com>
2024-08-11 11:23:09 +0900

    [level 2] Title: 피보나치 수, Time: 2.46 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,11 +1,10 @@
 class Solution {
-    
     public int solution(int n) {
-        int[] fibo = new int[n+1];
+        int[] fibo = new int[n + 1];
         fibo[1] = 1;
         
-        for(int i=2;i<fibo.length;i++){
-            fibo[i] = (fibo[i-1]+fibo[i-2])%1234567;
+        for(int i = 2; i < fibo.length; i++){
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
         }
         
         int answer = fibo[n];
