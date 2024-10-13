1aadf42edc0bc7f951daf73dd66c9414dc0ab956
Bonkri <139411343+bonkri9@users.noreply.github.com>
2024-10-13 14:12:36 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,14 +1,34 @@
-select 
+select
     i.id,
     n.fish_name,
     i.length
-from fish_info i
-join fish_name_info n
-on i.fish_type = n.fish_type
-where i.length = (
+from fish_info i, fish_name_info n
+where 
+    i.fish_type = n.fish_type
+    and
+    (i.fish_type, i.length) 
+    in
+    (
     select 
-        max(i2.length)
-    from fish_info i2
-    where i.fish_type = i2.fish_type
-)
-order by 1;
+        fish_type,
+        max(length)
+    from fish_info
+    group by fish_type
+    )
+order by i.id asc;
+    
+
+# select 
+#     i.id,
+#     n.fish_name,
+#     i.length
+# from fish_info i
+# join fish_name_info n
+# on i.fish_type = n.fish_type
+# where i.length = (
+#     select 
+#         max(i2.length)
+#     from fish_info i2
+#     where i.fish_type = i2.fish_type
+# )
+# order by 1;
