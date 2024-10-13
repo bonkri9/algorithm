8a5af72cacd5824af847d09c979af38d42bfec03
Bonkri <139411343+bonkri9@users.noreply.github.com>
2024-10-13 10:04:09 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -5,10 +5,10 @@ select
     last_name
 from developer_infos
 where
-    skill_1 = 'Python'
-    or
-    skill_2 = 'Python'
-    or
-    skill_3 = 'Python'
+    'Python' in (skill_1, skill_2, skill_3)
+    # skill_1 = 'Python'
+    # or
+    # skill_2 = 'Python'
+    # or
+    # skill_3 = 'Python'
 order by id asc;
-    
