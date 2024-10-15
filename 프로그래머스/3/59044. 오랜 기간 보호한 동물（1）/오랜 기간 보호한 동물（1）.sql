832128a1d6fd8ca495411168103c6188b81892b3
Bonkri <139411343+bonkri9@users.noreply.github.com>
2024-10-15 12:48:10 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,25 @@
-SELECT
-    i.name,
-    i.datetime
+select
+    name,
+    datetime
 from animal_ins i
-left join animal_outs o 
-on i.animal_id = o.animal_id
-where
-    o.animal_id is null
+where 
+    i.animal_id
+    not in
+    (
+        select animal_id
+        from animal_outs
+    )
 order by i.datetime asc
 limit 3;
+
+
+# SELECT
+#     i.name,
+#     i.datetime
+# from animal_ins i
+# left join animal_outs o 
+# on i.animal_id = o.animal_id
+# where
+#     o.animal_id is null
+# order by i.datetime asc
+# limit 3;
