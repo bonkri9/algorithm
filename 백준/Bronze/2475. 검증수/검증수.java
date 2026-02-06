5a84492adb6f48be54cd07fedcc7ea62f3d5cdf0
Bonkri <139411343+bonkri9@users.noreply.github.com>
2026-02-06 16:22:58 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
-		int e = sc.nextInt();
-		
-		System.out.println((a*a+b*b+c*c+d*d+e*e)%10);
-			
-		}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-	
+		int sum = 0;
+		while (st.hasMoreTokens() ) {
+			int a = Integer.parseInt(st.nextToken());
+			sum += a * a;
+		}
+		
+		System.out.println(sum % 10);
+	}
 
 }
