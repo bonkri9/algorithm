fb7f3209a65c25f7178956c2118e8f460c1f955e
Bonkri <139411343+bonkri9@users.noreply.github.com>
2026-02-06 16:34:45 +0900

    [Bronze V] Title: 구구단, Time: 108 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
-		for (int i = 1; i <= 9; i++) {
-			System.out.println(N+" * "+i+" = " + N * i);
+		StringBuilder sb = new StringBuilder();
+		for (int i=1;i<=9;i++) {
+			sb.append(N).append(" * ").append(i).append(" = ").append(N * i).append("\n");
 		}
+		
+		System.out.println(sb);
 	}
+
 }
