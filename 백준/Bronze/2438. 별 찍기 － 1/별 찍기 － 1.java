53686e3850982473d0069fee7281a9b91692f598
Bonkri <139411343+bonkri9@users.noreply.github.com>
2026-02-06 16:14:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 92 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
+
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j <= i; j++) {
+				sb.append("*");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		
+		System.out.println(sb);
 	}
+
 }
