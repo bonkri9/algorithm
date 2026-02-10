5bd32be8548d242a2c2fa8ea7e93403df7b21900
Bonkri <139411343+bonkri9@users.noreply.github.com>
2026-02-10 11:40:27 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -3,31 +3,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	
+	static boolean[] primeChecker(int num) {
+		boolean[] isPrime = new boolean[num + 1];
+		Arrays.fill(isPrime, true);
+		isPrime[0] = isPrime[1] = false;
+		
+		for (int i = 0; i * i <= num; i++) {
+			if (!isPrime[i]) continue;
+			for (int j = i * i; j <= num; j += i) {
+				isPrime[j] = false;
+			}
+		}
+		
+		return isPrime;
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] arr = new int[N];
-		for(int i=0;i<arr.length;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
 		
+		boolean[] isPrime = primeChecker(1000);
+
 		int cnt = 0;
-		for(int i=0;i<arr.length;i++) {
-			boolean flag = false;
-			if(arr[i] < 2) continue;
-			
-			for(int j=2;j<=Math.sqrt(arr[i]);j++) {
-				if(arr[i] % j == 0) {
-					flag = true;
-					break;
-				};
-			}
-			
-			if(!flag) cnt++;
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (isPrime[num]) cnt++;
 		}
 		
 		System.out.println(cnt);
