51fbda38ea5a5a6c0dfd124cda34e547ebf08aca
Bonkri <139411343+bonkri9@users.noreply.github.com>
2026-02-24 14:43:28 +0900

    [Silver V] Title: 나이순 정렬, Time: 404 ms, Memory: 40816 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -3,37 +3,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static class Member {
-		int age;
-		String name;
-		
-		Member(int age, String name) {
-			this.age = age;
-			this.name = name;
-		}
-	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int N = Integer.parseInt(br.readLine());
 		
-		List<Member> list = new ArrayList<>();
-		StringTokenizer st;
-		for (int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
+		List<List<String>> buckets = new ArrayList<>();
+		for (int i = 0; i <= 200; i++) {
+		    buckets.add(new ArrayList<>());
+		}
+		
+		for(int i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			int age = Integer.parseInt(st.nextToken());
 			String name = st.nextToken();
 			
-			list.add(new Member(age, name));
+			buckets.get(age).add(name);
 		}
 		
-		Collections.sort(list, (a, b) -> a.age - b.age);
-		
-		for(Member m : list) {
-			System.out.println(m.age + " " + m.name);
+		StringBuilder sb = new StringBuilder();
+		for(int age = 1;age <=200; age++) {
+			for(String name : buckets.get(age)) {
+				sb.append(age).append(" ").append(name).append("\n");
+			}
 		}
+		
+		System.out.println(sb);
 	}
 
 }
